ff47017bf8045bec0d0872c55132ad6cd7df770c
John Ratana <jratana1@gmail.com>
2020-12-17 21:28:43 -0500

    commit tree 4b27036581498b64f5f00f0b9e8ee3b0a76d4b64

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,42 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+ end
+
+ def genre_id=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_id
+    self.genre ? self.genre.name : nil
+  end
+
+  def notes=(notes)
+    notes.each do |note|
+      note = Note.create(content: note)
+      self.notes << note
+    end
+  end
+
+  def note_contents
+    self.notes.collect(&:content)
+  end
+
+  def note_contents=(note_contents)
+    note_contents.each do |content|
+      unless content.empty?
+        self.notes << Note.create(content: content)
+        self.save
+      end
+    end
+  end
 end
